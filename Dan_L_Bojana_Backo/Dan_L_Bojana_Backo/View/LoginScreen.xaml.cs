aaaf8266bb056e4ffcbeaae167c7de0e4d94c0a4
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-08-05 11:42:04 +0200

    Add LoginScreen.xaml, Add LoginScreenViewModel.cs

---
+++ Dan_L_Bojana_Backo/Dan_L_Bojana_Backo/View/LoginScreen.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Dan_L_Bojana_Backo.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dan_L_Bojana_Backo.View
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+            this.DataContext = new LoginScreenViewModel(this);
+        }
+    }
+}
---
+++ Dan_L_Bojana_Backo/Dan_L_Bojana_Backo/ViewModel/LoginScreenViewModel.cs
@@ -0,0 +1,90 @@
+﻿using Dan_L_Bojana_Backo.Command;
+using Dan_L_Bojana_Backo.View;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Dan_L_Bojana_Backo.ViewModel
+{
+    class LoginScreenViewModel : ViewModelBase
+    {
+        LoginScreen loginScreen;
+
+        public LoginScreenViewModel(LoginScreen loginScreenOpen)
+        {
+            loginScreen = loginScreenOpen;
+        }
+
+        private string userName;
+        public string UserName
+        {
+
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        public string Error
+        {
+            get { return null; }
+        }
+
+        public string this[string someProperty]
+        {
+            get
+            {
+
+                return string.Empty;
+            }
+        }
+
+        private ICommand submit;
+        public ICommand Submit
+        {
+            get
+            {
+                if (submit == null)
+                {
+                    submit = new RelayCommand(SubmitCommandExecute,
+                        param => CanSubmitCommandExecute());
+                }
+                return submit;
+            }
+        }
+
+        private void SubmitCommandExecute(object obj)
+        {
+            try
+            {
+                string password = (obj as PasswordBox).Password;
+                
+                if (UserName.Equals("Zaposleni") && password.Equals("Zaposleni"))
+                {
+                    MainWindow employeeView = new MainWindow();
+                    loginScreen.Close();
+                    employeeView.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong usename or password");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private bool CanSubmitCommandExecute()
+        {
+            return true;
+        }
+    }
+}
