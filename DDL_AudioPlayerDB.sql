2bf58b1f8aaf8334c596aa33821f7c31d38e5ea4
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-08-05 15:33:40 +0200

    Update DDL

--- DDL_AudioPlayerDB.sql
+++ DDL_AudioPlayerDB.sql
@@ -4,13 +4,13 @@ GO
 USE AudioPlayerDB;
 GO
 
-IF OBJECT_ID('tblUser', 'U') IS NOT NULL DROP TABLE tblUser;
 IF OBJECT_ID('tblSong', 'U') IS NOT NULL DROP TABLE tblSong;
+IF OBJECT_ID('tblUser', 'U') IS NOT NULL DROP TABLE tblUser;
 
 CREATE TABLE tblUser (
     UserID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
     Username nvarchar(50) NOT NULL,
-	Password nvarchar(50) NOT NULL,
+	Password nvarchar(150) NOT NULL,
 );
 
 CREATE TABLE tblSong (
